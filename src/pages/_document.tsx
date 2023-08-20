aad2680e1ceb66132a9b92ee09bb23b0da71a2be
ohtani shimpei <ohtani.shimpei@gmail.com>
2023-08-20 10:08:00 +0900

    update function & css

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,7 @@ export default function Document() {
 			<Head>
 				<meta name="description" content="Pokemon Typing Game by PokeAPI" />
 				<meta name="google-site-verification" content="n9PDU346DViaXjImHRDlZeCU0X0w95-TYQwpbWoTD9A" />
+				<link rel="preload" href="https://fonts.gstatic.com/s/josefinsans/v26/Qw3PZQNVED7rKGKxtqIqX5E-AVSJrOCfjY46_N_XbMZhKSbpUVzEEQ.woff" as="font" type="font/woff" crossOrigin="anonymous" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
 				<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:wght@700&display=swap" rel="stylesheet" />
